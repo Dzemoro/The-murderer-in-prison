e54161bd67079d71da12ef61b7b95ea7737db2e2
CatOnXTC <karolsienkiewicz03@gmail.com>
2024-08-12 22:02:20 +0200

    Hide notebook while not open

--- Assets/sNotebook.cs
+++ Assets/sNotebook.cs
@@ -8,22 +8,25 @@ public class sNotebook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
- 
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (_player.GetComponent<sMovement>()._freeToAct && Input.GetKeyDown(KeyCode.Q) && !GetComponent<CanvasGroup>().interactable)
         {
             GetComponent<CanvasGroup>().alpha = 1.0f;
             GetComponent<CanvasGroup>().interactable = true;
+            transform.localScale = new Vector3(1f, 1f, 1f);
             _player.GetComponent<sMovement>()._freeToAct = false;
         }
         else if (!_player.GetComponent<sMovement>()._freeToAct && Input.GetKeyDown(KeyCode.Q) && GetComponent<CanvasGroup>().interactable)
         {
             GetComponent<CanvasGroup>().alpha = 0.0f;
             GetComponent<CanvasGroup>().interactable = false;
+            transform.localScale = new Vector3(0f, 1f, 1f);
             _player.GetComponent<sMovement>()._freeToAct = true;
         }
     }
