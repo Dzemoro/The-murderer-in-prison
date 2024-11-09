e471de5a64fd4856b9547ae16be17c4ac08ee22f
Dzemoro <dellorpl@gmail.com>
2024-11-09 01:08:49 +0100

    Added assets and animations to prison guards

--- Assets/Scripts/sGuard.cs
+++ Assets/Scripts/sGuard.cs
@@ -1,50 +1,46 @@
 using DialogueEditor;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class sGuard : MonoBehaviour
 {
     [SerializeField] private NPCConversation Conversation;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
     [SerializeField] private float speed;
     [SerializeField] private float leftPatrolX;
     [SerializeField] private float rightPatrolX;
-    [SerializeField] private float minPauseTime;
-    [SerializeField] private float maxPauseTime;
-    [SerializeField] private float minWalkTime;
-    [SerializeField] private float maxWalkTime;
     [SerializeField] private int facingDirection = -1;
 
-    private float _randomTime;
-    public float _timer;
     private bool _isWalking = true;
     private bool _playerIsClose;
     private bool _isGuardAbleToMove = true;
     private bool _isChangingDirection;
-    private float _lastX;
 
     // Start is called before the first frame update
     void Start()
     {
-        _randomTime = Random.Range(minWalkTime, maxWalkTime);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Animate();
         if (Input.GetKeyDown(KeyCode.E) && _playerIsClose && !ConversationManager.Instance.IsConversationActive && sMovement.Instance.FreeToAct)
         {
             StopGuardMovement();
             sMovement.Instance.StopMovement();
             ConversationManager.Instance.StartConversation(Conversation);
         }
-
     }
 
     private void FixedUpdate()
     {
+        Animate();
         if (_isGuardAbleToMove)
         {
+
             if (transform.position.x > rightPatrolX || transform.position.x < leftPatrolX)
                 facingDirection *= -1;
 
@@ -67,6 +63,21 @@ public class sGuard : MonoBehaviour
         }
     }
 
-    public void StopGuardMovement() => _isGuardAbleToMove = false;
-    public void StartGuardMovement() => _isGuardAbleToMove = true;
+    public void StopGuardMovement()
+    {
+        _isGuardAbleToMove = false;
+        _isWalking = false;
+    }
+    public void StartGuardMovement()
+    {
+        _isGuardAbleToMove = true;
+        _isWalking = true;
+    }
+
+    private void Animate()
+    {
+        if (_isWalking)
+            animator.SetFloat("X", facingDirection);
+        animator.SetBool("Moving", _isWalking);
+    }
 }
